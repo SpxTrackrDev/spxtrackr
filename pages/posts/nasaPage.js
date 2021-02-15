698e3e5827ecd179c6dfeeec095c8e09efac51a4
Theveak Brandon Chea <brandonchea231@gmail.com>
2021-02-14 23:55:14 -0800

    updated front end on NASA page

--- pages/posts/nasaPage.js
+++ pages/posts/nasaPage.js
@@ -1,18 +1,28 @@
 import Link from 'next/link'
 
+
 export default function nasa_Page(){
     return (
      <>
-     <h1>Nasa Page</h1>
-     <h2>
-         <Link href="/">
-             <a>Back to front page.</a>
-         </Link>
-     </h2>
-     <h2>
-        How the hell do I make the link blue
-     </h2>
-
+     <div className='page'>
+     <div className='headerParent' >
+        <div className='header'>
+            <h1>Nasa Tracker</h1>
+        </div>
+     </div>
+     <div className='box'>
+    <div className='launch'>
+        <h2>
+            <Link href="/">
+                <a>Back to front page.</a>
+            </Link>
+        </h2>
+        <h2>
+            NASA information coming soon
+        </h2>
+    </div>
+    </div>
+    </div>
      </>
     )
     
