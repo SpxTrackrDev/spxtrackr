f75dff9dc23d84164c4aa1d4868df28adb4ef45b
flashruler <44584657+flashruler@users.noreply.github.com>
2021-02-15 23:30:20 -0800

    Revert "updated front end on NASA page"
    This reverts commit 698e3e5827ecd179c6dfeeec095c8e09efac51a4.

--- pages/posts/nasaPage.js
+++ pages/posts/nasaPage.js
@@ -1,28 +1,18 @@
 import Link from 'next/link'
 
-
 export default function nasa_Page(){
     return (
      <>
-     <div className='page'>
-     <div className='headerParent' >
-        <div className='header'>
-            <h1>Nasa Tracker</h1>
-        </div>
-     </div>
-     <div className='box'>
-    <div className='launch'>
-        <h2>
-            <Link href="/">
-                <a>Back to front page.</a>
-            </Link>
-        </h2>
-        <h2>
-            NASA information coming soon
-        </h2>
-    </div>
-    </div>
-    </div>
+     <h1>Nasa Page</h1>
+     <h2>
+         <Link href="/">
+             <a>Back to front page.</a>
+         </Link>
+     </h2>
+     <h2>
+        How the hell do I make the link blue
+     </h2>
+
      </>
     )
     
