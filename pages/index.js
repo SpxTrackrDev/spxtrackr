af71e2658d213fc6fe21e19cf3d1bf7c7fdab564
flashruler <jbuens001@gmail.com>
2021-03-07 13:04:58 -0800

    Changed titles

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import React, { Component } from "react"
+import Countdown from 'react-countdown';
 const axios = require('axios');
 
 export default class extends Component {
@@ -63,55 +64,39 @@ export default class extends Component {
         <div className='menuParent'>
           <div className='menuButton'>
             <h2>
-              <Link href="/posts/nasaPage">
-                <a>NASA</a>
-              </Link>
+              <a className='title' href="/posts/nasaPage">
+                NASA
+              </a>
             </h2>
           </div>
           <div className='menuButton'>
             <h2>
-              <Link href="/posts/spxPage">
-                <a>SpaceX</a>
-              </Link>
+              <a className='title' href="/posts/spxPage">
+                SpaceX
+              </a>
             </h2>
           </div>
         </div>
+        <div className='launch'>
+          <h1>Upcoming Launches</h1>
+        </div>
         {this.state.launches && this.state.launches.map((value, index) => {
-          if (index < 1) {
+          if (index < 2) {
             // const divStyle = { backgroundImage: 'url(https://wallpaperaccess.com/full/1145374.jpg)' }
             let date = new Date(value.date_local)
-            var deadline = date;
-            var x = setInterval(function () {
-              let now = new Date().getTime();
-              var t = deadline - now;
-              var days = Math.floor(t / (1000 * 60 * 60 * 24));
-              var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-              var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-              var seconds = Math.floor((t % (1000 * 60)) / 1000);
-              document.getElementById("demo").innerHTML = days + "d "
-                + hours + "h " + minutes + "m " + seconds + "s ";
-              if (t < 1) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "LAUNCHED/LIVE";
-              }
-            }, 1000);
+            //im not entirely sure how to make this better
+            
             return (
               <div className='box' key={index} >
                 <div className='timer' onClick={() => this.handleClick(index)}>
-                <h1>{value.name}</h1>
-                  <h2 id="demo"></h2>
+                <h1>Upcoming Launch: {value.name}</h1>                 
                 </div>
-                {/* <div className='launch' onClick={() => this.handleClick(index)} >
-                  <h1>{value.name}</h1>
-                </div> */}
                 {this.state.showInfo === index &&
                   <div className='info'>
                     <h1> Launch Info</h1>
                     <p className='paragraph'>{value.details}</p>
                     <h3>Flight No. : {value.flight_number}</h3>
                     <h3>Date: {date.toString()}</h3>
-                    {/* <h2>Booster No. : {()=>this.getBooster(value.cores[0].core)}</h2> */}
-                    {/* {console.log(value.cores[0].core)} */}
                   </div>}
               </div>
             )
