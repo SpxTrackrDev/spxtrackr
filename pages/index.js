4ac2e4e2e2d1b8a2734040c65f846c77f0056126
flashruler <44584657+flashruler@users.noreply.github.com>
2021-06-08 21:53:55 -0700

    reverted to countdown

--- pages/index.js
+++ pages/index.js
@@ -63,19 +63,20 @@ export default class extends Component {
         <div className='menuParent'>
           <div className='menuButton'>
             <h2>
-              <a className='title' href="/posts/nasaPage">
-                NASA
-              </a>
+              <Link href="/posts/nasaPage">
+                <a>NASA</a>
+              </Link>
             </h2>
           </div>
           <div className='menuButton'>
             <h2>
-              <a className='title' href="/posts/spxPage">
-                SpaceX
-              </a>
+              <Link href="/posts/spxPage">
+                <a>SpaceX</a>
+              </Link>
             </h2>
           </div>
         </div>
+<<<<<<< HEAD
         <div className='countdown'>
 
         </div>
@@ -84,10 +85,13 @@ export default class extends Component {
           <p1>These are the two upcoming spaceX launches.</p1>
           <p1> Select one to view additional information</p1>
         </div>
+=======
+>>>>>>> parent of af71e26 (Changed titles)
         {this.state.launches && this.state.launches.map((value, index) => {
-          if (index < 2) {
+          if (index < 1) {
             // const divStyle = { backgroundImage: 'url(https://wallpaperaccess.com/full/1145374.jpg)' }
             let date = new Date(value.date_local)
+<<<<<<< HEAD
             let currentDate =  new Date()
             let launchlink = this.getLaunchpad(value.launchpad)
             //im not entirely sure how to make this better
@@ -96,14 +100,44 @@ export default class extends Component {
               <div className='box' key={index} >
                 <div className='timer' onClick={() => this.handleClick(index)}>
                 <h1>Upcoming Launch: {value.name}</h1> 
+=======
+            var deadline = date;
+            var x = setInterval(function () {
+              let now = new Date().getTime();
+              var t = deadline - now;
+              var days = Math.floor(t / (1000 * 60 * 60 * 24));
+              var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+              var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+              var seconds = Math.floor((t % (1000 * 60)) / 1000);
+              document.getElementById("demo").innerHTML = days + "d "
+                + hours + "h " + minutes + "m " + seconds + "s ";
+              if (t < 1) {
+                clearInterval(x);
+                document.getElementById("demo").innerHTML = "LAUNCHED/LIVE";
+              }
+            }, 1000);
+            return (
+              <div className='box' key={index} >
+                <div className='timer' onClick={() => this.handleClick(index)}>
+                <h1>{value.name}</h1>
+                  <h2 id="demo"></h2>
+>>>>>>> parent of af71e26 (Changed titles)
                 </div>
+                {/* <div className='launch' onClick={() => this.handleClick(index)} >
+                  <h1>{value.name}</h1>
+                </div> */}
                 {this.state.showInfo === index &&
                   <div className='info'>
                     <h1> Launch Info</h1>
                     <p className='paragraph'>{value.details}</p>
                     <h3>Flight No. : {value.flight_number}</h3>
                     <h3>Date: {date.toString()}</h3>
+<<<<<<< HEAD
                     {/* <h3>Launchpad: {console.log(launchlink)}</h3> */}
+=======
+                    {/* <h2>Booster No. : {()=>this.getBooster(value.cores[0].core)}</h2> */}
+                    {/* {console.log(value.cores[0].core)} */}
+>>>>>>> parent of af71e26 (Changed titles)
                   </div>}
               </div>
             )
