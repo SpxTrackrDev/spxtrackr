c20c42671c8c16d111263afaba1971dd807df13d
flashruler <44584657+flashruler@users.noreply.github.com>
2021-06-09 22:36:15 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 import React, { Component } from "react"
 const axios = require('axios');
 
- 
 export default class extends Component {
   state = {
     launches: [],
@@ -11,27 +13,23 @@ export default class extends Component {
     super(props)
     this.getData = this.getData.bind(this)
     this.handleClick = this.handleClick.bind(this)
-    this.getBooster = this.getLaunchpad(this)
+    // this.getBooster = this.getBooster(this)
   }
   componentDidMount() {
     this.getData()
   }
-  getLaunchpad(launchpad)
-  {
-    let link = 'https://api.spacexdata.com/v4/launchpads/'
-    let pad=''
-    console.log(launchpad)
-    axios.get(link.concat(launchpad))
-      .then(function (response) {
-        console.log(link.concat(launchpad))
-        console.log(response.data)
-        return response.data.serial
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-  }
+  // getBooster(core)
+  // {
+  //   console.log(core)
+  //   axios.get('https://api.spacexdata.com/v4/cores/'+core)
+  //     .then(function (response) {
+  //       return response.data.serial
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  // }
   getData() {
     let that = this
     axios.get('https://api.spacexdata.com/v4/launches/upcoming')
@@ -51,6 +49,8 @@ export default class extends Component {
     this.setState(state => ({ showInfo: index }))
   }
 
+
+
   render() {
     return (
 
@@ -63,59 +63,23 @@ export default class extends Component {
         <div className='menuParent'>
           <div className='menuButton'>
             <h2>
-              <a className='title' href="/posts/nasaPage">
-                NASA
-              </a>
+              <Link href="/posts/nasaPage">
+                <a>NASA</a>
+              </Link>
             </h2>
           </div>
           <div className='menuButton'>
             <h2>
-              <a className='title' href="/posts/spxPage">
-                SpaceX
-              </a>
+              <Link href="/posts/spxPage">
+                <a>SpaceX</a>
+              </Link>
             </h2>
           </div>
         </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 4ac2e4e (reverted to countdown)
-        <div className='countdown'>
-
-        </div>
-=======
->>>>>>> parent of 925a0b7 (Added new CSS)
-        <div className='launch'>
-          <h1>Upcoming Launches</h1>
-        </div>
-<<<<<<< HEAD
-=======
->>>>>>> parent of af71e26 (Changed titles)
-=======
->>>>>>> parent of af71e26 (Changed titles)
-=======
->>>>>>> parent of 4ac2e4e (reverted to countdown)
         {this.state.launches && this.state.launches.map((value, index) => {
-          if (index < 2) {
+          if (index < 1) {
             // const divStyle = { backgroundImage: 'url(https://wallpaperaccess.com/full/1145374.jpg)' }
             let date = new Date(value.date_local)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 4ac2e4e (reverted to countdown)
-            let currentDate =  new Date()
-            let launchlink = this.getLaunchpad(value.launchpad)
-            //im not entirely sure how to make this better
-            
-            return (
-              <div className='box' key={index} >
-                <div className='timer' onClick={() => this.handleClick(index)}>
-                <h1>Upcoming Launch: {value.name}</h1> 
-<<<<<<< HEAD
-=======
             var deadline = date;
             var x = setInterval(function () {
               let now = new Date().getTime();
@@ -135,58 +99,19 @@ export default class extends Component {
               <div className='box' key={index} >
                 <div className='timer' onClick={() => this.handleClick(index)}>
                 <h1>{value.name}</h1>
-                  <h2 id="demo"></h2>
->>>>>>> parent of af71e26 (Changed titles)
-=======
-            var deadline = date;
-            var x = setInterval(function () {
-              let now = new Date().getTime();
-              var t = deadline - now;
-              var days = Math.floor(t / (1000 * 60 * 60 * 24));
-              var hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-              var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-              var seconds = Math.floor((t % (1000 * 60)) / 1000);
-              document.getElementById("demo").innerHTML = days + "d "
-                + hours + "h " + minutes + "m " + seconds + "s ";
-              if (t < 1) {
-                clearInterval(x);
-                document.getElementById("demo").innerHTML = "LAUNCHED/LIVE";
-              }
-            }, 1000);
-            return (
-              <div className='box' key={index} >
-                <div className='timer' onClick={() => this.handleClick(index)}>
-                <h1>{value.name}</h1>
-                  <h2 id="demo"></h2>
->>>>>>> parent of af71e26 (Changed titles)
-=======
->>>>>>> parent of 4ac2e4e (reverted to countdown)
+                  {/* <h2 id="demo"></h2> */}
                 </div>
+                {/* <div className='launch' onClick={() => this.handleClick(index)} >
+                  <h1>{value.name}</h1>
+                </div> */}
                 {this.state.showInfo === index &&
                   <div className='info'>
                     <h1> Launch Info</h1>
                     <p className='paragraph'>{value.details}</p>
                     <h3>Flight No. : {value.flight_number}</h3>
                     <h3>Date: {date.toString()}</h3>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    {/* <h3>Launchpad: {console.log(launchlink)}</h3> */}
-=======
                     {/* <h2>Booster No. : {()=>this.getBooster(value.cores[0].core)}</h2> */}
                     {/* {console.log(value.cores[0].core)} */}
->>>>>>> parent of af71e26 (Changed titles)
-=======
-                    {/* <h2>Booster No. : {()=>this.getBooster(value.cores[0].core)}</h2> */}
-                    {/* {console.log(value.cores[0].core)} */}
->>>>>>> parent of af71e26 (Changed titles)
-=======
-                    {/* <h3>Launchpad: {console.log(launchlink)}</h3> */}
->>>>>>> parent of 4ac2e4e (reverted to countdown)
-=======
-                    <h3>Launchpad: {console.log(launchlink)}</h3>
->>>>>>> parent of 925a0b7 (Added new CSS)
                   </div>}
               </div>
             )
